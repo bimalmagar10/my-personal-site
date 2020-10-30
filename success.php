c8dcd216b2b75d4dca3f678f48a3fce0e99f3572
bimalmagar10 <bimalmagar873@gmail.com>
2020-10-30 11:15:40 +0545

    some modifications

--- success.php
+++ success.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge
+	<meta name=viewport content="width=device-width, initial-scale=1">
 	<title>Bimal Thapa Magar | Thank You</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
